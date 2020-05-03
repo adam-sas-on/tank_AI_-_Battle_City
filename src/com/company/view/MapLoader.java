2c9881985497c11eaf9bb553752ffc530a543426
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-05-03 12:48:45 +0200

    Fix blocking cells for bigger objects

--- src/com/company/view/MapLoader.java
+++ src/com/company/view/MapLoader.java
@@ -36,8 +36,8 @@ public class MapLoader {
 			rows = scan.nextInt();
 
 
-		//boolean[] freeLowerCells = new boolean[cols];
-		boolean freeNextCell;
+		boolean[] freeCells = new boolean[(cols + 1)*2];
+		int futureRowIndex = cols + 1, currentRowIndex = 0;
 
 		scan.nextLine();
 
@@ -48,20 +48,22 @@ public class MapLoader {
 		for(row = 0; row < rows && rowCellBegin != null; row++){
 			line = scan.nextLine();
 			stepCell = rowCellBegin;
-			freeNextCell = true;
+
 
 			for(col = 0; col < cols && stepCell != null; col++){
 				stepCell.setMapCell(null);
 				if(col >= line.length())
 					break;
 
-				if(!freeNextCell /*|| !freeLowerCells[col]*/){
+				if(!freeCells[currentRowIndex + col]){
 					stepCell = stepCell.getRightCell();
-					freeNextCell = true;
+					freeCells[currentRowIndex + col] = true;
 					continue;
 				}
 
-				freeNextCell = true;
+
+				freeCells[currentRowIndex + col] = true;
+				freeCells[currentRowIndex + col + 1] = true;
 				switch(line.charAt(col)){
 					case '#':
 						stepCell.setMapCell(MapCell.STEEL);
@@ -108,44 +110,47 @@ public class MapLoader {
 						break;
 					case 'I':
 						stepCell.setMapCell(MapCell.ICE);
-						freeNextCell = false;
-						//falseArrayIndexes(freeLowerCells, col);
+						freeCells[currentRowIndex + col + 1] = false;
+						freeCells[futureRowIndex + col] = freeCells[futureRowIndex + col + 1] = false;
 						break;
 					case 'F':
 						stepCell.setMapCell(MapCell.FOREST);
-						freeNextCell = false;
-						//falseArrayIndexes(freeLowerCells, col);
 						trees.add(stepCell.getId());
+						freeCells[currentRowIndex + col + 1] = false;
+						freeCells[futureRowIndex + col] = freeCells[futureRowIndex + col + 1] = false;
 						break;
 					case 'W':
 						stepCell.setMapCell(MapCell.WATER);
-						freeNextCell = false;
-						//falseArrayIndexes(freeLowerCells, col);
+						freeCells[currentRowIndex + col + 1] = false;
+						freeCells[futureRowIndex + col] = freeCells[futureRowIndex + col + 1] = false;
 						break;
 					case 'E':
 						stepCell.setMapCell(MapCell.EAGLE);
-						freeNextCell = false;
-						//falseArrayIndexes(freeLowerCells, col);
+						freeCells[currentRowIndex + col + 1] = false;
+						freeCells[futureRowIndex + col] = freeCells[futureRowIndex + col + 1] = false;
 						break;
 					case '1':
 						playerCol = stepCell.getCol();
 						playerRow = stepCell.getRow();
 						player1.getCell().setPos(playerCol, playerRow);
-						freeNextCell = false;
-						//falseArrayIndexes(freeLowerCells, col);
+						freeCells[currentRowIndex + col + 1] = false;
+						freeCells[futureRowIndex + col] = freeCells[futureRowIndex + col + 1] = false;
 						break;
 					case '2':
 						playerCol = stepCell.getCol();
 						playerRow = stepCell.getRow();
 						player2.getCell().setPos(playerCol, playerRow);
-						freeNextCell = false;
-						//falseArrayIndexes(freeLowerCells, col);
+						freeCells[currentRowIndex + col + 1] = false;
+						freeCells[futureRowIndex + col] = freeCells[futureRowIndex + col + 1] = false;
 						break;
 				}
 				stepCell = stepCell.getRightCell();
 			}
 
 			rowCellBegin = rowCellBegin.getDownCell();
+			col = futureRowIndex;// SWAP(currentRowIndex, futureRowIndex);
+			futureRowIndex = currentRowIndex;
+			currentRowIndex = col;
 		}
 	}
 
