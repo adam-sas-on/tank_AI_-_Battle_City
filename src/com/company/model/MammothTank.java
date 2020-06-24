b529e7a209ce5d166564b8536bbf3e3aa8adbafd
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-06-24 23:14:41 +0200

    Add most armored enemy tank

---
+++ src/com/company/model/MammothTank.java
@@ -0,0 +1,38 @@
+package com.company.model;
+
+import com.company.logic.BattleRandom;
+import com.company.view.GameView;
+import com.company.view.MapCell;
+
+public class MammothTank extends Enemy {
+	public MammothTank(BattleRandom rand, GameView view){
+		super(rand, view);
+		level = 4;
+	}
+
+	public MammothTank(BattleRandom rand, GameView view, boolean powerApp){
+		super(rand, view, powerApp);
+		level = 4;
+	}
+
+	@Override
+	protected void setIcons(boolean containsPowerUp){
+		MapCell[] cells;
+		Direction direction = Direction.DOWN;
+
+		cells = MapCell.mammothTankDownState(level, containsPowerUp);
+
+		if(icons.isEmpty() )
+			currentIcons = cells;
+		icons.put(direction.getDirection(), cells);
+
+		direction = Direction.UP;
+		icons.put(direction.getDirection(), MapCell.mammothTankUpState(level, containsPowerUp) );
+
+		direction = Direction.RIGHT;
+		icons.put(direction.getDirection(), MapCell.mammothTankRightState(level, containsPowerUp) );
+
+		direction = Direction.LEFT;
+		icons.put(direction.getDirection(), MapCell.mammothTankLeftState(level, containsPowerUp) );
+	}
+}
